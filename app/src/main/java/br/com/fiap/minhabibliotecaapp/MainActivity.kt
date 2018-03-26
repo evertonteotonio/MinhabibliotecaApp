831a553affb391b42afdb1aca3654fb4cdfb9f58
logonrm <logonrm@fiap.com.br>
2018-03-26 20:46:20 -0300

    first commit

--- app/src/main/java/br/com/fiap/minhabibliotecaapp/MainActivity.kt
+++ app/src/main/java/br/com/fiap/minhabibliotecaapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import br.com.fiap.minhabiblioteca.Calculadora
+import br.com.fiap.minhabibliotecaapp.R
 
 class MainActivity : AppCompatActivity() {
 
